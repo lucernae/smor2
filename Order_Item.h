48a2cc313aead45473310ab0a6e6d6305e37ced8
lana.pcfre@gmail.com <lana.pcfre@gmail.com@ce1152ac-b3a1-b03a-0f24-ae7c1841c481>
2011-11-08 15:13:37 +0000

    [B3] Show menu works, added Category and Order Item menu bar... Also help menu now call a new window to display help window...

--- Order_Item.h
+++ Order_Item.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <sstream>
+#include <iomanip> // RMN add include
 using namespace std;
 
 
@@ -13,9 +14,15 @@ public:
 	Order_Item(string sid, int oid, int miid, int n):
 	seat_id(sid), order_id(oid), menu_item_id(miid), prod_qty(n) {}
    // utility functions
+	static string display_headers() {
+		ostringstream oss;
+		oss << left << setw(9) << "Seat ID" << left << setw(10) << "Order ID" << left << setw(13) << "Menu Item ID" << left << setw(9) << "Quantity";
+		return oss.str();
+	}
 	string display() const {	
 		ostringstream oss;
-		oss << seat_id << " " << order_id << " " << menu_item_id << " " << prod_qty ;
+		//oss << seat_id << " " << order_id << " " << menu_item_id << " " << prod_qty ;
+		oss << left << setw(9) << seat_id << left << setw(10) << order_id << left << setw(13) << menu_item_id << left << setw(9) << prod_qty;
 		return oss.str();
 	}
 	int get_order_id() const {return order_id;}	//B.2.a
